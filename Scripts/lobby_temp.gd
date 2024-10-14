4046b56dc4077efb33f286d8c0cd76db745317ba
ThePancakeMan521 <159019381+ThePancakeMan521@users.noreply.github.com>
2024-10-14 11:19:58 +0200

    Minor changes

--- Scripts/lobby_temp.gd
+++ Scripts/lobby_temp.gd
@@ -4,3 +4,7 @@ extends Node2D
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/test_world.tscn")
