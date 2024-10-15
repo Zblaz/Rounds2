ac74ad5ca6c80a55307549d531435155f7e624b5
ThePancakeMan521 <gradygamer5212@gmail.com>
2024-10-15 13:05:55 +0200

    moved back

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -9,7 +9,7 @@ func _physics_process(delta: float):
 	look_at(mouse_position)
 func load_bullet(delta: float) :
 	preload("res://Scenes/bullet.gd")
-
+	
 var bullet_start = self.position
 #var bullet_direction = 
 var draw_circle_flag = false
@@ -27,5 +27,5 @@ func _draw():
 		draw_circle(bullet_start, radius, Color(0,0,1))
 
 func _on_bullets_area_entered(area: StaticBody2D):
-	var draw_circle_flag = false
+	var _draw_circle_flag = false
 	print('collide')
